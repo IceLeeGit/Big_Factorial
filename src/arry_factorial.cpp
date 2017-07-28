f484b560339f892941ca2c11f3fc42ab5c0566f3
liyubing <liyubing@opzoon.com>
2017-07-28 07:40:42 -0400

    Modify the arry factorial. Add the vector funtion to storage the value of factorial.

--- src/arry_factorial.cpp
+++ src/arry_factorial.cpp
@@ -1,47 +1,61 @@
 #include <iostream>  
-#define N 3000         
 #include<cstring>  
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 
 using namespace std;  
 
-int a[N];     //定义数组  
 int main(int argc,char** argv)  
 {  
-    int n,i,j,c,s;  
+    int num;  
     if (argc==0)
     {
         cout<<"You need enter a integer: ";
-        cin>>n;  
+        cin>>num;  
     }
     else
-        n = atoi(argv[1]);
+        num = atoi(argv[1]);
 
-    clock_t time_start,time_end; 
+    vector<int> sum;
+    int count,temp;
+    clock_t time_start,time_end;
     time_start = clock();
 
-    cout<<"The "<<n<<" number's factorial is: ";
-    memset(a,0,sizeof(a));      //数组清零  
-    a[0]=1;                             //a[0]赋值为1  
-    for(j=2;j<=n;j++)   //乘以j  
-    {  
-       c=0;                    //是否进位  
-       for(i=0;i<N;i++)  
-       {  
-           s=a[i]*j+c;  
-           a[i]=s%10;  
-           c=s/10;  
-       }  
-    }  
-    for(i=N-1;i>=0;i--)  
-       if(a[i])break;             //忽略前导零  
-    for(j=i;j>=0;j--)      //倒序输出  
-       cout<<a[j];  
+    cout<<"The "<<num<<" number's factorial is: ";
+    sum.push_back(1);
+    for(int i=2;i<=num;i++)
+    {
+        count = 0;
+        for(int j=0;j<sum.size();j++)
+        {
+            temp = sum[j]*i+count;
+            sum[j] = temp%10;
+            count = temp/10;
+        }
+
+        if(0<count && count<10)
+        {
+            sum.push_back(count);
+        }
+        else if(count>=10)
+        {
+            int divisor = count;
+            do
+            {
+                sum.push_back(divisor%10);
+                divisor = divisor/10;
+            }while(divisor>=10);
+            sum.push_back(divisor);
+        }
+    }
+
+    for(vector<int>::reverse_iterator it=sum.rbegin();it!=sum.rend();it++)
+        cout<<*it;
       
     cout<<endl;  
     time_end = clock();
-    cout<<"Run times are "<<(double)(time_end-time_start)/CLOCKS_PER_SEC<<"s."<<endl;
+    cout<<"Run times are "<<(long)(time_end-time_start)/CLOCKS_PER_SEC<<"s."<<endl;
     return 0;  
 }  
